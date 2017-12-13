544ed1b2ed34f59a81bc418f7dd106d7f24f6e71
voaignidc <1171595505@qq.com>
2017-12-13 15:06:55 +0800

    now can show PressureDiagram

--- ui/pressureAnalysis.py
+++ ui/pressureAnalysis.py
@@ -13,9 +13,9 @@ import matplotlib.pyplot as plt
 class PressureData():
     """计算压力,压强等"""
     def __init__(self):
-        self.voltageArr = np.array(self.readVoltageArrFromTxt())
-        self.forceArr = self.voltageArrToForceArr(self.voltageArr)
-        self.pressureArr = self.forceArrToPressureArr(self.forceArr)
+        self.voltageArr = np.array(self.readVoltageArrFromTxt()) # 电压列表
+        self.forceArr = self.voltageArrToForceArr(self.voltageArr) # 压力列表
+        self.pressureArr = self.forceArrToPressureArr(self.forceArr) # 压强列表
 
         self.totalForce = self.forceArr.sum() # 总压力
         self.averageForce = int(self.totalForce/(44*32)) # 平均压力
@@ -24,6 +24,12 @@ class PressureData():
         self.maxPressure = np.max(self.pressureArr) # 最大压强
         self.minPressure = np.min(self.pressureArr[self.pressureArr != 0]) # 最小有效压强
         self.touchedArea = self.getTouchedArea(self.voltageArr) # 接触面积
+        self.pressureDistribution=[]
+        for i in range(10):
+            bigger = self.pressureArr[self.pressureArr >= i*1000]
+            among = bigger[bigger < (i+1)*1000]
+            self.pressureDistribution.append(len(among))
+        self.pressureDistribution = np.array(self.pressureDistribution) # 压强分布
 
     def readVoltageArrFromTxt(self):
         """
@@ -90,45 +96,103 @@ class PressureData():
 
 class PressureDiagram():
     """压强分析图"""
-    def __init__(self, pressureArr):
+    def __init__(self, pressureDistribution):
         super().__init__()
-        X = [0, 1, 2, 3, 4, 5]
-        Y = [222, 42, 455, 664, 454, 334]
-        fig = plt.figure()
-        plt.bar(X, Y, 0.4, color="blue")
-        plt.xlabel("pressure(kPa)")
-        plt.ylabel("times")
-        plt.title("pressure-times hist")
-        plt.show()
+        self.pressureDistribution = pressureDistribution
+        self.fig = plt.figure(1)
+        self.fig.set_size_inches(8, 4) # 设置图片大小
+        self.barAx = self.fig.add_subplot(121)
+        self.pieAx = self.fig.add_subplot(122)
+        self.drawPressureTimesBar()
+        self.drawPressureTimesPie()
+
+    def savePressureDiagram(self):
+        """保存压强分布图"""
+
+        plt.savefig("pressureDiagram.png")
+
+    def drawPressureTimesBar(self):
+        """压强分布柱形图"""
+        width = 0.5
+        xticks = np.linspace(0.5, 9.5, 10)
+        xticklabels = []
+        for i in range(10):
+            xticklabels.append(str(i+1)+'k') # 获得标签
+
+        self.barAx.bar(xticks-width/2, self.pressureDistribution, width, color="blue")
+        self.barAx.set_xticks(xticks)
+        self.barAx.set_xticklabels(xticklabels)
+        self.barAx.set_xlabel("pressure(kPa)")
+        self.barAx.set_ylabel("times")
+        self.barAx.set_title("pressure-times bar")
+
+    def drawPressureTimesPie(self):
+        """压强分布扇形图"""
+        zeroIndex = np.where(self.pressureDistribution == 0)
+        labels = []
+        for i in np.arange(10): # 获得压强值非0的标签
+            if False in np.isnan(np.where(zeroIndex == i)):
+                pass
+            else:
+                labels.append(str(i)+'k-'+str(i+1)+'k')
+
+        self.pieAx.pie(self.pressureDistribution[self.pressureDistribution > 0], labels = labels, autopct='%1.1f%%')
+        self.pieAx.set_title("pressure-times pie")
 
 class PressureAnalysisDlg(QDialog):
     """压力分析的对话框"""
     def __init__(self):
         super().__init__()
         self.setupUi()
-        self.setupLayout()
         self.showUi()
-        pressureData = PressureData()
-        pressureDiagram = PressureDiagram(pressureData.pressureArr)
+        self.pressureData = PressureData()
+        self.refreshLineEdit()
+        pressureDiagram = PressureDiagram(self.pressureData.pressureDistribution)
+        pressureDiagram.savePressureDiagram()
+        self.showPressureDiagram()
+        self.setupLayout()
 
+    def showPressureDiagram(self):
+        self.pressureImage = QImage()
+        self.pressureImageLabel = QLabel(self)
+        self.refreshPressureDiagram()
 
+    def refreshPressureDiagram(self):
+        """刷新压强分析图"""
+        if self.pressureImage.load("./pressureDiagram.png"):
+            self.pressureImageLabel.setPixmap(QPixmap.fromImage(self.pressureImage))
+
+    def refreshLineEdit(self):
+        self.totalForceLineEdit.setText(str(self.pressureData.totalForce))
+        self.averageForceLineEdit.setText(str(self.pressureData.averageForce))
+        self.totalPressureLineEdit.setText(str(self.pressureData.totalPressure))
+        self.averagePressureLineEdit.setText(str(self.pressureData.averagePressure))
+        self.maxPressureLineEdit.setText(str(self.pressureData.maxPressure))
+        self.minPressureLineEdit.setText(str(self.pressureData.minPressure))
+        self.touchedAreaLineEdit.setText(str(self.pressureData.touchedArea))
 
     def setupUi(self):
         self.totalForceLabel = QLabel("总压力(N)", self)
         self.totalForceLineEdit = QLineEdit(self)
-        self.totalForceLineEdit.setEnabled(False)
+        self.totalForceLineEdit.setFixedWidth(100)
         self.averageForceLabel = QLabel("平均压力(N)", self)
         self.averageForceLineEdit = QLineEdit(self)
+        self.averageForceLineEdit.setFixedWidth(100)
         self.totalPressureLabel = QLabel("总压强(kPa)", self)
         self.totalPressureLineEdit = QLineEdit(self)
+        self.totalPressureLineEdit.setFixedWidth(100)
         self.averagePressureLabel = QLabel("平均压强(kPa)", self)
         self.averagePressureLineEdit = QLineEdit(self)
+        self.averagePressureLineEdit.setFixedWidth(100)
         self.maxPressureLabel = QLabel("最大压强(kPa)", self)
         self.maxPressureLineEdit = QLineEdit(self)
+        self.maxPressureLineEdit.setFixedWidth(100)
         self.minPressureLabel = QLabel("最小压强(kPa)", self)
         self.minPressureLineEdit = QLineEdit(self)
+        self.minPressureLineEdit.setFixedWidth(100)
         self.touchedAreaLabel = QLabel("接触面积(cm^2)", self)
         self.touchedAreaLineEdit = QLineEdit(self)
+        self.touchedAreaLineEdit.setFixedWidth(100)
 
     def setupLayout(self):
         self.gridLayout = QGridLayout()
@@ -149,7 +213,12 @@ class PressureAnalysisDlg(QDialog):
         self.gridLayout.addWidget(self.touchedAreaLabel, *(6,0))
         self.gridLayout.addWidget(self.touchedAreaLineEdit, *(6,1))
 
-        self.setLayout(self.gridLayout)
+        mainLayout = QHBoxLayout()
+        mainLayout.addStretch(1)
+        mainLayout.addLayout(self.gridLayout)
+        mainLayout.addWidget(self.pressureImageLabel)
+        self.setLayout(mainLayout)
+
 
     def showUi(self):
         self.setWindowFlags(Qt.WindowCloseButtonHint) # 关闭问号
